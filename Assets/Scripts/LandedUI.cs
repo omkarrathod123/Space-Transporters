6854acb4657ee0acb06e590f2abc6c26d38f1c9c
omkarrathod123 <omkarrathod0705@gmail.com>
2026-07-27 16:27:51 +0530

    Game Over Script draft - 1 is ready

--- Assets/Scripts/LandedUI.cs
+++ Assets/Scripts/LandedUI.cs
@@ -5,4 +5,32 @@ public class LandedUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI bannerTitleText;
     [SerializeField] private TextMeshProUGUI dataText;
+
+    private void Start()
+    {
+        Lander.Instance.onLanded += Lander_onLanded;
+        GameOver(false);
+    }
+
+    private void Lander_onLanded(object sender, Assets.Scripts.onLandedEventArgs e)
+    {
+        if(e.landingType == Lander.LandingType.Success)
+        {
+            bannerTitleText.text = "SUCCESSFUL LANDING!";
+        }
+        else
+        {
+            bannerTitleText.text = "LANDING FAILED!";
+        }
+        dataText.text =
+            Mathf.Round(e.landingSpeed * 1.3f) + "\n" +
+            Mathf.Round(e.landingAngle) + "\n" +
+            e.scoreMultiplier + "\n" +
+            e.score;
+        GameOver(true);
+    }
+    private void GameOver(bool b)
+    {
+        gameObject.SetActive(b);
+    }
 }
